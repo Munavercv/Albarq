1a2236bb80ff208a93df762e867c1c8c2e124c65
Munavercv <munavermunu80@gmail.com>
2024-08-21 21:55:03 +0530

    homepage perfomance boosted

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,22 +34,6 @@
         }
     });
 
-    //Main navigation Active Class Add Remove
-    // $(".navbar-toggler").on("click", function () {
-    //     $("header").toggleClass("active");
-    // });
-    // $(document).on("ready", function () {
-    //     if ($(window).width() > 991) {
-    //         $("header").removeClass("active");
-    //     }
-    //     $(window).on("resize", function () {
-    //         if ($(window).width() > 991) {
-    //             $("header").removeClass("active");
-    //         }
-    //     });
-    // });
-
-        // menu js
 
 
 
